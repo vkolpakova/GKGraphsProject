92457ed298eb278547eee817e0a7810783d37697
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2014-12-11 17:43:41 +0500

    refs #28 FieldAut first commit

---
+++ GKGraphs/src/Kernel/GraphConstructor/Series/FieldAutSeriesGraphConstructor.java
@@ -0,0 +1,31 @@
+package Kernel.GraphConstructor.Series;
+
+import Kernel.Graph.PrimeNumberGraph;
+import Kernel.Group.Series;
+
+public class FieldAutSeriesGraphConstructor extends AutSubgroupSeriesGraphConstructor {
+
+	public FieldAutSeriesGraphConstructor(Series group) {
+		super(group);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void computeCentralizationsMap() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public PrimeNumberGraph constructGKGraph() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
