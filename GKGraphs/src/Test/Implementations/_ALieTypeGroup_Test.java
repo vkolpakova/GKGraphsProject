d4e0a24ac14aae31b24d39b8bc81740bd09e5af1
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-03-02 19:23:11 +0500

    Add test to 6-primary concrete _A type groups

--- GKGraphs/src/Test/Implementations/_ALieTypeGroup_Test.java
+++ GKGraphs/src/Test/Implementations/_ALieTypeGroup_Test.java
@@ -57,4 +57,42 @@ public class _ALieTypeGroup_Test extends AbstractLieTypeGroup_Test {
 		PrimeNumberGraph gr = new PrimeNumberGraph(vList, eList);
 		Assert.assertTrue(gr.equals(graph));
 	}
+	
+	private static final String _A_4_4 		= "_A_4(4)";
+	private static final String V__A_4_4 	= "2,3,5,13,17,41";
+	private static final String E__A_4_4 	= "{2,3},{2,5},{2,13},{3,5},{3,13},{3,17},{5,13}";
+	
+	@Test
+	public void test__A_4_4() {
+		Assert.assertTrue(super.checkEquality(_A_4_4, V__A_4_4, E__A_4_4));
+	}
+	
+	private static final String _A_4_5 		= "_A_4(5)";
+	private static final String V__A_4_5 	= "2,3,5,7,13,521";
+	private static final String E__A_4_5 	= "{2,3},{2,5},{2,7},{2,13},{3,5},{3,7},{3,13},{5,7}";
+	
+	@Test
+	public void test__A_4_5() {
+		Assert.assertTrue(super.checkEquality(_A_4_5, V__A_4_5, E__A_4_5));
+	}
+	
+	private static final String _A_4_9 		= "_A_4(9)";
+	private static final String V__A_4_9 	= "2,3,5,41,73,1181";
+	private static final String E__A_4_9 	= "{2,3},{2,5},{2,41},{2,73},{3,5},{3,73},{5,73}";
+	
+	@Test
+	public void test__A_4_9() {
+		Assert.assertTrue(super.checkEquality(_A_4_9, V__A_4_9, E__A_4_9));
+	}
+	
+	private static final String _A_5_3 		= "_A_5(3)";
+	private static final String V__A_5_3 	= "2,3,5,7,13,61";
+	private static final String E__A_5_3 	= "{2,3},{2,5},{2,7},{2,13},{3,5},{3,7},{3,13},{7,13}";
+	
+	@Test
+	public void test__A_5_3() {
+		// FIXME неудача -  лишнее ребро ({3,13})
+		Assert.assertTrue(super.checkEquality(_A_5_3, V__A_5_3, E__A_5_3));
+	}
+	
 }
