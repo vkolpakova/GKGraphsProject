ab73135baaaecb69e7cb0d0a66520615d3d2a359
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-03-04 12:08:23 +0500

    Add tests for concrete 6-primes group of D type

--- GKGraphs/src/Test/Implementations/DLieTypeGroup_Test.java
+++ GKGraphs/src/Test/Implementations/DLieTypeGroup_Test.java
@@ -31,4 +31,43 @@ public class DLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
 		Assert.assertTrue(gr.equals(graph));
 	}
 	
+	private static final String D_4_4 = "D_4(4)";
+	private static final String V_D_4_4 = "2,3,5,7,13,17";
+	private static final String E_D_4_4 = "{2,3},{2,5},{2,17},{3,5},{3,7},{3,17},{5,7},{5,13},{5,17}";
+	
+	@Test
+	public void testD_4_4() {
+		// FIXME лишнее ребро {5,7}
+		Assert.assertTrue(super.checkEquality(D_4_4, V_D_4_4, E_D_4_4));
+	}
+	
+	private static final String D_4_5 = "D_4(5)";
+	private static final String V_D_4_5 = "2,3,5,7,13,31";
+	private static final String E_D_4_5 = "{2,3},{2,5},{2,13},{3,5},{3,7},{3,13},{3,31},{5,13}";
+	
+	@Test
+	public void testD_4_5() {
+		// FIXME лишнее ребро {3,31}, не хватает ребра {2,31}
+		Assert.assertTrue(super.checkEquality(D_4_5, V_D_4_5, E_D_4_5));
+	}
+	
+	private static final String D_4_7 = "D_4(7)";
+	private static final String V_D_4_7 = "2,3,5,7,19,43";
+	private static final String E_D_4_7 = "{2,3},{2,5},{2,7},{3,5},{3,7},{3,19},{3,43},{5,7}";
+	
+	@Test
+	public void testD_4_7() {
+		// FIXME лишнее ребро {3,43}, не хватает ребра {2,19}
+		Assert.assertTrue(super.checkEquality(D_4_7, V_D_4_7, E_D_4_7));
+	}
+	
+	private static final String D_5_2 = "D_5(2)";
+	private static final String V_D_5_2 = "2,3,5,7,17,31";
+	private static final String E_D_5_2 = "{2,3},{2,5},{2,7},{3,5},{3,7},{3,17}";
+	
+	@Test
+	public void testD_5_2() {
+		Assert.assertTrue(super.checkEquality(D_5_2, V_D_5_2, E_D_5_2));
+	}
+	
 }
