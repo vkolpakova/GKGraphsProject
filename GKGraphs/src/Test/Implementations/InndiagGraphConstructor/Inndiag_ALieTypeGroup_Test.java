b20ef35a860aaac6113b6b888547a5ea8294961f
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-03-06 16:28:26 +0600

    Add Inndiag 6-primes concrete groups of U type (tests)

---
+++ GKGraphs/src/Test/Implementations/InndiagGraphConstructor/Inndiag_ALieTypeGroup_Test.java
@@ -0,0 +1,88 @@
+package Test.Implementations.InndiagGraphConstructor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import Test.Implementations._ATypableConcrete;
+
+public class Inndiag_ALieTypeGroup_Test extends AbstractInndiagLieTypeGroup_Test implements _ATypableConcrete {
+	
+	@Test
+	public void test__A_4_4() {
+		Assert.assertTrue(super.checkEquality(_A_4_4, V__A_4_4, I_E__A_4_4));
+	}
+	
+	@Test
+	public void test__A_4_5() {
+		Assert.assertTrue(super.checkEquality(_A_4_5, V__A_4_5, I_E__A_4_5));
+	}
+	
+	@Test
+	public void test__A_4_9() {
+		// FIXME Error В оригинальном графе отсутствует ребро {5,1181}
+		Assert.assertTrue(super.checkEquality(_A_4_9, V__A_4_9, I_E__A_4_9));
+	}
+	
+	@Test
+	public void test__A_5_3() {
+		// FIXME Error В оригинальном графе лишнее ребро {3,13}
+		Assert.assertTrue(super.checkEquality(_A_5_3, V__A_5_3, I_E__A_5_3));
+	}
+	
+	@Test
+	public void test__A_6_2() {
+		Assert.assertTrue(super.checkEquality(_A_6_2, V__A_6_2, I_E__A_6_2));
+	}
+	
+	@Test
+	public void test__A_2_512() {
+		Assert.assertTrue(super.checkEquality(_A_2_512, V__A_2_512, I_E__A_2_512));
+	}
+	
+	@Test
+	public void test__A_2_27() {
+		Assert.assertTrue(super.checkEquality(_A_2_27, V__A_2_27, I_E__A_2_27));
+	}
+	
+	@Test
+	public void test__A_2_125() {
+		Assert.assertTrue(super.checkEquality(_A_2_125, V__A_2_125, I_E__A_2_125));
+	}
+	
+	@Test
+	public void test__A_2_625() {
+		Assert.assertTrue(super.checkEquality(_A_2_625, V__A_2_625, I_E__A_2_625));
+	}
+	
+	@Test
+	public void test__A_2_49() {
+		Assert.assertTrue(super.checkEquality(_A_2_49, V__A_2_49, I_E__A_2_49));
+	}
+	
+	@Test
+	public void test__A_2_343() {
+		// FIXME Error В оригинальном графе лишнее ребро {3,7}
+		Assert.assertTrue(super.checkEquality(_A_2_343, V__A_2_343, I_E__A_2_343));
+	}
+	
+	@Test
+	public void test__A_2_289() {
+		Assert.assertTrue(super.checkEquality(_A_2_289, V__A_2_289, I_E__A_2_289));
+	}
+	
+	@Test
+	public void test__A_3_8() {
+		Assert.assertTrue(super.checkEquality(_A_3_8, V__A_3_8, I_E__A_3_8));
+	}
+	
+	@Test
+	public void test__A_3_16() {
+		Assert.assertTrue(super.checkEquality(_A_3_16, V__A_3_16, I_E__A_3_16));
+	}
+	
+	@Test
+	public void test__A_3_25() {
+		Assert.assertTrue(super.checkEquality(_A_3_25, V__A_3_25, I_E__A_3_25));
+	}
+	
+}
