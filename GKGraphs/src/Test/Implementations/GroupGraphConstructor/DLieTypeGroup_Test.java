380a46cc6a594db4a010bd57ef324d79aea49c9f
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-03-06 15:48:19 +0600

    Fix concrete 6-primes groups of D type (tests)

--- GKGraphs/src/Test/Implementations/GroupGraphConstructor/DLieTypeGroup_Test.java
+++ GKGraphs/src/Test/Implementations/GroupGraphConstructor/DLieTypeGroup_Test.java
@@ -11,8 +11,9 @@ import Kernel.Graph.Edge;
 import Kernel.Graph.PrimeNumberGraph;
 import Kernel.Graph.SimpleVertex;
 import Kernel.Graph.Vertex;
+import Test.Implementations.DTypableConcrete;
 
-public class DLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
+public class DLieTypeGroup_Test extends AbstractLieTypeGroup_Test implements DTypableConcrete {
 	
 	private static final String NAME_1 = "D_5(2)";
 	
@@ -31,39 +32,20 @@ public class DLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
 		Assert.assertTrue(gr.equals(graph));
 	}
 	
-	private static final String D_4_4 = "D_4(4)";
-	private static final String V_D_4_4 = "2,3,5,7,13,17";
-	private static final String E_D_4_4 = "{2,3},{2,5},{2,17},{3,5},{3,7},{3,17},{5,7},{5,13},{5,17}";
-	
 	@Test
 	public void testD_4_4() {
-		// FIXME лишнее ребро {5,7}
 		Assert.assertTrue(super.checkEquality(D_4_4, V_D_4_4, E_D_4_4));
 	}
 	
-	private static final String D_4_5 = "D_4(5)";
-	private static final String V_D_4_5 = "2,3,5,7,13,31";
-	private static final String E_D_4_5 = "{2,3},{2,5},{2,13},{3,5},{3,7},{3,13},{3,31},{5,13}";
-	
 	@Test
 	public void testD_4_5() {
-		// FIXME лишнее ребро {3,31}, не хватает ребра {2,31}
 		Assert.assertTrue(super.checkEquality(D_4_5, V_D_4_5, E_D_4_5));
 	}
 	
-	private static final String D_4_7 = "D_4(7)";
-	private static final String V_D_4_7 = "2,3,5,7,19,43";
-	private static final String E_D_4_7 = "{2,3},{2,5},{2,7},{3,5},{3,7},{3,19},{3,43},{5,7}";
-	
 	@Test
 	public void testD_4_7() {
-		// FIXME лишнее ребро {3,43}, не хватает ребра {2,19}
 		Assert.assertTrue(super.checkEquality(D_4_7, V_D_4_7, E_D_4_7));
 	}
-	
-	private static final String D_5_2 = "D_5(2)";
-	private static final String V_D_5_2 = "2,3,5,7,17,31";
-	private static final String E_D_5_2 = "{2,3},{2,5},{2,7},{3,5},{3,7},{3,17}";
 	
 	@Test
 	public void testD_5_2() {
