cd45f631420ff9ebf2bad87476ced18f87915227
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-03-06 14:26:17 +0600

    Fix concrete 6-primes groups of _A type (tests)

---
+++ GKGraphs/src/Test/Implementations/_ATypableConcrete.java
@@ -0,0 +1,80 @@
+package Test.Implementations;
+
+public interface _ATypableConcrete {
+	
+	// U_5(4)
+	public static final String _A_4_4 		= "_A_4(4)";
+	public static final String V__A_4_4 	= "2,3,5,13,17,41";
+	public static final String E__A_4_4 	= "{2,3},{2,5},{2,13},{3,5},{3,13},{3,17},{5,13}";
+	
+	// U_5(5)
+	public static final String _A_4_5 		= "_A_4(5)";
+	public static final String V__A_4_5 	= "2,3,5,7,13,521";
+	public static final String E__A_4_5 	= "{2,3},{2,5},{2,7},{2,13},{3,5},{3,7},{3,13},{5,7}";
+	
+	// U_5(9)
+	public static final String _A_4_9 		= "_A_4(9)";
+	public static final String V__A_4_9 	= "2,3,5,41,73,1181";
+	public static final String E__A_4_9 	= "{2,3},{2,5},{2,41},{2,73},{3,5},{3,73},{5,73}";
+	
+	// U_6(3)
+	public static final String _A_5_3 		= "_A_5(3)";
+	public static final String V__A_5_3 	= "2,3,5,7,13,61";
+	public static final String E__A_5_3 	= "{2,3},{2,5},{2,7},{2,61},{3,5},{3,7},{7,13}";
+	
+	// U_7(2)
+	public static final String _A_6_2 		= "_A_6(2)";
+	public static final String V__A_6_2 	= "2,3,5,7,11,43";
+	public static final String E__A_6_2 	= "{2,3},{2,5},{2,11},{3,5},{3,7},{3,11}";
+	
+	// U_3(512)
+	public static final String _A_2_512 	= "_A_2(512)";
+	public static final String V__A_2_512 	= "2,3,7,19,73,87211";
+	public static final String E__A_2_512 	= "{2,3},{2,19},{3,7},{3,19},{3,73},{7,19},{7,73},{19,73}";
+	
+	// U_3(27)
+	public static final String _A_2_27 		= "_A_2(27)";
+	public static final String V__A_2_27 	= "2,3,7,13,19,37";
+	public static final String E__A_2_27 	= "{2,3},{2,7},{2,13},{3,7},{7,13},{19,37}";
+	
+	// U_3(125)
+	public static final String _A_2_125 	= "_A_2(125)";
+	public static final String V__A_2_125 	= "2,3,5,7,31,5167";
+	public static final String E__A_2_125 	= "{2,3},{2,5},{2,7},{2,31},{3,5},{3,7},{3,31},{5,7},{7,31}}";
+	
+	// U_3(625)
+	public static final String _A_2_625 	= "_A_2(625)";
+	public static final String V__A_2_625 	= "2,3,5,13,313,390001";
+	public static final String E__A_2_625 	= "{2,3},{2,5},{2,13},{2,313},{3,13},{3,313},{5,313},{13,313}";
+	
+	// U_3(49)
+	public static final String _A_2_49 		= "_A_2(49)";
+	public static final String V__A_2_49 	= "2,3,5,7,13,181";
+	public static final String E__A_2_49 	= "{2,3},{2,5},{2,7},{3,5},{5,7},{13,181}";
+	
+	// U_3(343)
+	public static final String _A_2_343 	= "_A_2(343)";
+	public static final String V__A_2_343 	= "2,3,7,19,43,117307";
+	public static final String E__A_2_343 	= "{2,3},{2,7},{2,19},{2,43},{3,19},{3,43},{7,43},{19,43}";
+	
+	// U_3(289)
+	public static final String _A_2_289 	= "_A_2(289)";
+	public static final String V__A_2_289 	= "2,3,5,17,29,83233";
+	public static final String E__A_2_289 	= "{2,3},{2,5},{2,17},{2,29},{3,5},{3,29},{5,17},{5,29},{17,29}";
+	
+	// U_4(8)
+	public static final String _A_3_8 		= "_A_3(8)";
+	public static final String V__A_3_8 	= "2,3,5,7,13,19";
+	public static final String E__A_3_8 	= "{2,3},{2,7},{3,7},{3,19},{5,7},{5,13},{7,13}";
+	
+	// U_4(16)
+	public static final String _A_3_16 		= "_A_3(16)";
+	public static final String V__A_3_16 	= "2,3,5,17,241,257";
+	public static final String E__A_3_16 	= "{2,3},{2,5},{2,17},{3,5},{3,5},{3,257},{5,17},{5,257},{17,241}";
+	
+	// U_4(25)
+	public static final String _A_3_25 		= "_A_3(25)";
+	public static final String V__A_3_25 	= "2,3,5,13,313,601";
+	public static final String E__A_3_25 	= "{2,3},{2,5},{2,13},{2,313},{3,5},{3,13},{3,313},{5,13},{13,601}";
+	
+}
