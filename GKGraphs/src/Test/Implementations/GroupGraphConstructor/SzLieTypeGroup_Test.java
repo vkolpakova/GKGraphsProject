ee3b9019fcb33b0e443df8eca4bc1c879cf737f7
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-03-07 18:45:14 +0600

    Add Inndiag 6-primes concrete groups of Sz type (tests)

--- GKGraphs/src/Test/Implementations/GroupGraphConstructor/SzLieTypeGroup_Test.java
+++ GKGraphs/src/Test/Implementations/GroupGraphConstructor/SzLieTypeGroup_Test.java
@@ -10,8 +10,9 @@ import com.google.common.collect.Lists;
 import Kernel.Graph.Edge;
 import Kernel.Graph.PrimeNumberGraph;
 import Kernel.Graph.Vertex;
+import Test.Implementations.SzTypableConcrete;
 
-public class SzLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
+public class SzLieTypeGroup_Test extends AbstractLieTypeGroup_Test implements SzTypableConcrete {
 	
 	private static final String NAME_1 = "Sz(8)";
 	private static final String NAME_2 = "Sz(32)";
@@ -32,6 +33,16 @@ public class SzLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
 		List<Vertex<?>> vList = computeVList();
 		PrimeNumberGraph gr = new PrimeNumberGraph(vList, eList);
 		Assert.assertTrue(gr.equals(graph));
+	}
+	
+	@Test
+	public void testSz_8() {
+		Assert.assertTrue(super.checkEquality(SZ_8, V_SZ_8, E_SZ_8));
+	}
+	
+	@Test
+	public void testSz_32() {
+		Assert.assertTrue(super.checkEquality(SZ_32, V_SZ_32, E_SZ_32));
 	}
 	
 }
---
+++ GKGraphs/src/Test/Implementations/SzTypableConcrete.java
@@ -0,0 +1,17 @@
+package Test.Implementations;
+
+public interface SzTypableConcrete {
+	
+	// Sz(8)
+	public static final String SZ_8 		= "Sz(8)";
+	public static final String V_SZ_8 		= "2,5,7,13";
+	public static final String E_SZ_8 		= "";
+	public static final String I_E_SZ_8 	= "";
+	
+	// Sz(32)
+	public static final String SZ_32 		= "Sz(32)";
+	public static final String V_SZ_32 		= "2,5,31,41";
+	public static final String E_SZ_32 		= "";
+	public static final String I_E_SZ_32 	= "";
+	
+}
