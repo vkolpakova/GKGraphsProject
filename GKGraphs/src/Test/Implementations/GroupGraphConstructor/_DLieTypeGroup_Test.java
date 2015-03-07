94076fbda7c21c4ad5797885cc09909a5cf3ef1e
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-03-07 18:10:06 +0600

    Add Inndiag 6-primes concrete groups of 2D type (tests)

--- GKGraphs/src/Test/Implementations/GroupGraphConstructor/_DLieTypeGroup_Test.java
+++ GKGraphs/src/Test/Implementations/GroupGraphConstructor/_DLieTypeGroup_Test.java
@@ -11,8 +11,9 @@ import Kernel.Graph.Edge;
 import Kernel.Graph.PrimeNumberGraph;
 import Kernel.Graph.SimpleVertex;
 import Kernel.Graph.Vertex;
+import Test.Implementations._DTypableConcrete;
 
-public class _DLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
+public class _DLieTypeGroup_Test extends AbstractLieTypeGroup_Test implements _DTypableConcrete {
 	
 	private static final String NAME_1 = "_D_4(3)";
 	private static final String NAME_2 = "_D_5(2)";
@@ -46,4 +47,15 @@ public class _DLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
 		PrimeNumberGraph gr = new PrimeNumberGraph(vList, eList);
 		Assert.assertTrue(gr.equals(graph));
 	}
+	
+	@Test
+	public void test_D_4_3() {
+		Assert.assertTrue(super.checkEquality(_D_4_3, V__D_4_3, E__D_4_3));
+	}
+	
+	@Test
+	public void test_D_5_2() {
+		Assert.assertTrue(super.checkEquality(_D_5_2, V__D_5_2, E__D_5_2));
+	}
+	
 }
