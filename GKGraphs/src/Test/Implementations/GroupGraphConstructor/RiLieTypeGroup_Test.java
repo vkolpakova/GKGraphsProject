6faf8f9d10215ea2f047e9ab9daa32e17606406d
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-03-07 16:05:21 +0600

    Add Inndiag 6-primes concrete groups of Ri type (tests)

--- GKGraphs/src/Test/Implementations/GroupGraphConstructor/RiLieTypeGroup_Test.java
+++ GKGraphs/src/Test/Implementations/GroupGraphConstructor/RiLieTypeGroup_Test.java
@@ -11,8 +11,9 @@ import Kernel.Graph.Edge;
 import Kernel.Graph.PrimeNumberGraph;
 import Kernel.Graph.SimpleVertex;
 import Kernel.Graph.Vertex;
+import Test.Implementations.RiTypableConcrete;
 
-public class RiLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
+public class RiLieTypeGroup_Test extends AbstractLieTypeGroup_Test implements RiTypableConcrete {
 	
 	private static final String NAME_1 = "Ri(27)";
 	
@@ -26,6 +27,11 @@ public class RiLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
 		eList.add(new Edge(new SimpleVertex(2), new SimpleVertex(13)));
 		PrimeNumberGraph gr = new PrimeNumberGraph(vList, eList);
 		Assert.assertTrue(gr.equals(graph));
+	}
+	
+	@Test
+	public void testRi_27() {
+		Assert.assertTrue(super.checkEquality(RI_27, V_RI_27, E_RI_27));
 	}
 	
 }
---
+++ GKGraphs/src/Test/Implementations/RiTypableConcrete.java
@@ -0,0 +1,11 @@
+package Test.Implementations;
+
+public interface RiTypableConcrete {
+	
+	// Ri(27)
+	public static final String RI_27 		= "Ri(27)";
+	public static final String V_RI_27 		= "2,3,7,13,19,37";
+	public static final String E_RI_27 		= "{2,3},{2,7},{2,13}";
+	public static final String I_E_RI_27 	= "{2,3},{2,7},{2,13}";
+	
+}
