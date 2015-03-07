bc21d5b4d24b0beeaa92fd2f0f8816de00d8e2cc
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-03-07 13:58:06 +0600

    Add Inndiag 6-primes concrete groups of G type (tests)

--- GKGraphs/src/Test/Implementations/GroupGraphConstructor/GLieTypeGroup_Test.java
+++ GKGraphs/src/Test/Implementations/GroupGraphConstructor/GLieTypeGroup_Test.java
@@ -11,8 +11,9 @@ import Kernel.Graph.Edge;
 import Kernel.Graph.PrimeNumberGraph;
 import Kernel.Graph.SimpleVertex;
 import Kernel.Graph.Vertex;
+import Test.Implementations.GTypableConcrete;
 
-public class GLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
+public class GLieTypeGroup_Test extends AbstractLieTypeGroup_Test implements GTypableConcrete {
 	
 	private static final String NAME_1 = "G_2(9)";
 	private static final String NAME_2 = "G_2(17)";
@@ -43,6 +44,16 @@ public class GLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
 		eList.add(new Edge(new SimpleVertex(7), new SimpleVertex(13)));
 		PrimeNumberGraph gr = new PrimeNumberGraph(vList, eList);
 		Assert.assertTrue(gr.equals(graph));
+	}
+	
+	@Test
+	public void testG_2_9() {
+		Assert.assertTrue(super.checkEquality(G_2_9, V_G_2_9, E_G_2_9));
+	}
+	
+	@Test
+	public void testG_2_17() {
+		Assert.assertTrue(super.checkEquality(G_2_17, V_G_2_17, E_G_2_17));
 	}
 	
 }
