2bd7288dbc90b3114233297bb5fb4fa46a86cff7
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-03-07 13:39:56 +0600

    Add Inndiag 6-primes concrete groups of F type (tests)

--- GKGraphs/src/Test/Implementations/GroupGraphConstructor/FLieTypeGroup_Test.java
+++ GKGraphs/src/Test/Implementations/GroupGraphConstructor/FLieTypeGroup_Test.java
@@ -11,8 +11,9 @@ import Kernel.Graph.Edge;
 import Kernel.Graph.PrimeNumberGraph;
 import Kernel.Graph.SimpleVertex;
 import Kernel.Graph.Vertex;
+import Test.Implementations.FTypableConcrete;
 
-public class FLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
+public class FLieTypeGroup_Test extends AbstractLieTypeGroup_Test implements FTypableConcrete {
 	
 	private static final String NAME_1 = "F_4(2)";
 	
@@ -28,6 +29,11 @@ public class FLieTypeGroup_Test extends AbstractLieTypeGroup_Test {
 		eList.add(new Edge(new SimpleVertex(3), new SimpleVertex(7)));
 		PrimeNumberGraph gr = new PrimeNumberGraph(vList, eList);
 		Assert.assertTrue(gr.equals(graph));
+	}
+	
+	@Test
+	public void testF_4_2() {
+		Assert.assertTrue(super.checkEquality(F_4_2, V_F_4_2, E_F_4_2));
 	}
 	
 }
---
+++ GKGraphs/src/Test/Implementations/InndiagGraphConstructor/InndiagFLieTypeGroup_Test.java
@@ -0,0 +1,15 @@
+package Test.Implementations.InndiagGraphConstructor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import Test.Implementations.FTypableConcrete;
+
+public class InndiagFLieTypeGroup_Test extends AbstractInndiagLieTypeGroup_Test implements FTypableConcrete {
+	
+	@Test
+	public void testF_4_2() {
+		Assert.assertTrue(super.checkEquality(F_4_2, V_F_4_2, I_E_F_4_2));
+	}
+	
+}
