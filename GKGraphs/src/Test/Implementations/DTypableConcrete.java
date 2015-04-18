0eca1b9096af97c190b0ed7cf27fbb1727e96ba9
vkolpakova <Valeria.Kolpakova.VK@gmail.com>
2015-04-19 02:00:07 +0600

    Add full field aut tests for D type, concrete groups

--- GKGraphs/src/Test/Implementations/DTypableConcrete.java
+++ GKGraphs/src/Test/Implementations/DTypableConcrete.java
@@ -7,23 +7,27 @@ public interface DTypableConcrete {
 	public static final String V_D_4_4 		= "2,3,5,7,13,17";
 	public static final String E_D_4_4 		= "{2,3},{2,5},{2,17},{3,5},{3,7},{3,17},{5,13},{5,17}";
 	public static final String I_E_D_4_4 	= "{2,3},{2,5},{2,17},{3,5},{3,7},{3,17},{5,13},{5,17}";
+	public static final String F_E_D_4_4 	= "2#{2,3},{2,5},{2,7},{2,17},{3,5},{3,7},{3,17},{5,13},{5,17}";
 	
 	// D_4(5)
 	public static final String D_4_5 		= "D_4(5)";
 	public static final String V_D_4_5 		= "2,3,5,7,13,31";
 	public static final String E_D_4_5 		= "{2,3},{2,5},{2,13},{2,31},{3,5},{3,7},{3,13},{5,13}";
 	public static final String I_E_D_4_5 	= "{2,3},{2,5},{2,13},{2,31},{3,5},{3,7},{3,13},{5,13}";
+	public static final String F_E_D_4_5 	= E_D_4_5;
 	
 	// D_4(7)
 	public static final String D_4_7 		= "D_4(7)";
 	public static final String V_D_4_7 		= "2,3,5,7,19,43";
 	public static final String E_D_4_7 		= "{2,3},{2,5},{2,7},{2,19},{3,5},{3,7},{3,19},{5,7}";
 	public static final String I_E_D_4_7 	= "{2,3},{2,5},{2,7},{2,19},{3,5},{3,7},{3,19},{5,7}";
+	public static final String F_E_D_4_7 	= E_D_4_7;
 	
 	// D_5(2)
 	public static final String D_5_2 		= "D_5(2)";
 	public static final String V_D_5_2 		= "2,3,5,7,17,31";
 	public static final String E_D_5_2 		= "{2,3},{2,5},{2,7},{3,5},{3,7},{3,17}";
 	public static final String I_E_D_5_2 	= "{2,3},{2,5},{2,7},{3,5},{3,7},{3,17}";
+	public static final String F_E_D_5_2 	= E_D_5_2;
 	
 }
